2a66962748c0a471d51f7bcd08a6e4253cb4c8c6
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-05-20 13:35:25 -0400

    feature/get-connection: implemented

--- src/sqllize/index.js
+++ src/sqllize/index.js
@@ -1,3 +1,4 @@
 exports.getConnectionObject = require('./get-connection-object');
+exports.getConnection = require('./get-connection');
 exports.invokeSQLCmd = require('./invoke-sql-cmd');
 exports.bulkInsert = require('./bulk-insert');
